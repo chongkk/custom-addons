c674f501413f53da06c5884dc1a915ab4d87a6ec
chong <chong@alex.com.my>
2020-11-09 22:03:17 +0800

    modify

--- customfrpc/data/run.sh
+++ customfrpc/data/run.sh
@@ -1,72 +1,39 @@
-#!/usr/bin/env bashio
+#!/bin/bash
+set -e
 
-CERT_DIR=/data/letsencrypt
-WORK_DIR=/data/workdir
+CONFIG_PATH=/data/options.json
 
-# Let's encrypt
-LE_UPDATE="0"
+SERVER_IP=$(jq --raw-output '.server_ip' $CONFIG_PATH)
+AUTH_TOKEN=$(jq --raw-output '.auth_token // empty' $CONFIG_PATH)
+SERVER_PORT=$(jq --raw-output '.server_port' $CONFIG_PATH)
+LOCAL_PORT=$(jq --raw-output '.local_port' $CONFIG_PATH)
+REMOTE_PORT=$(jq --raw-output '.remote_port' $CONFIG_PATH)
+PROXY_NAME=$(jq --raw-output '.proxy_name // empty' $CONFIG_PATH)
 
-# DuckDNS
-IPV4=$(bashio::config 'ipv4 // empty')
-IPV6=$(bashio::config 'ipv6 // empty')
-TOKEN=$(bashio::config 'token')
-DOMAINS=$(bashio::config 'domains | join(",")')
-WAIT_TIME=$(bashio::config 'seconds')
-DUCKDNSDOMAIN=$(bashio::config 'duckdnsdomain | join(",")')
+FRP_PATH=/var/frp
+FRPC_CONF=$FRP_PATH/conf/frpc.ini
 
-# Function that performe a renew
-function le_renew() {
-    local domain_args=()
-    local domains
-  
-    domains=$(bashio::config 'domains')
-
-    # Prepare domain for Let's Encrypt
-    for domain in ${domains}; do
-        domain_args+=("--domain" "${domain}")
-    done
-    
-    dehydrated --cron --hook ./hooks.sh --challenge dns-01 "${domain_args[@]}" --out "${CERT_DIR}" --config "${WORK_DIR}/config" || true
-    LE_UPDATE="$(date +%s)"
-}
-
-# Register/generate certificate if terms accepted
-if bashio::config.true 'lets_encrypt.accept_terms'; then
-    # Init folder structs
-    mkdir -p "${CERT_DIR}"
-    mkdir -p "${WORK_DIR}"
-
-    # Clean up possible stale lock file
-    if [ -e "${WORK_DIR}/lock" ]; then
-        rm -f "${WORK_DIR}/lock"
-        bashio::log.warning "Reset dehydrated lock file"
-    fi
-
-    # Generate new certs
-    if [ ! -d "${CERT_DIR}/live" ]; then
-        # Create empty dehydrated config file so that this dir will be used for storage
-        touch "${WORK_DIR}/config"
-
-        dehydrated --register --accept-terms --config "${WORK_DIR}/config"
-    fi
+if [ -f $FRPC_CONF ]; then
+  rm $FRPC_CONF
 fi
 
-# Run duckdns
-while true; do
+if [ ! $PROXY_NAME ]; then
+  PROXY_NAME=hassio
+  echo Using default proxy name $PROXY_NAME
+fi
 
-    [[ ${IPV4} != *:/* ]] && ipv4=${IPV4} || ipv4=$(curl -s -m 10 "${IPV4}")
-    [[ ${IPV6} != *:/* ]] && ipv6=${IPV6} || ipv6=$(curl -s -m 10 "${IPV6}")
+echo "[common]" >> $FRPC_CONF
+echo "server_addr = $SERVER_IP" >> $FRPC_CONF
+echo "server_port = $SERVER_PORT" >> $FRPC_CONF
+if [ $AUTH_TOKEN ]; then
+  echo "privilege_token = $AUTH_TOKEN" >> $FRPC_CONF
+fi
+echo "[$PROXY_NAME]" >> $FRPC_CONF
+echo "type = tcp" >> $FRPC_CONF
+echo "local_ip = 127.0.0.1" >> $FRPC_CONF
+echo "local_port = $LOCAL_PORT" >> $FRPC_CONF
+echo "remote_port = $REMOTE_PORT" >> $FRPC_CONF
 
-    if answer="$(curl -s "https://www.duckdns.org/update?domains=${DUCKDNSDOMAIN}&token=${TOKEN}&ip=${ipv4}&ipv6=${ipv6}&verbose=true")"; then
-        bashio::log.info "${answer}"
-    else
-        bashio::log.warning "${answer}"
-    fi
-    
-    now="$(date +%s)"
-    if bashio::config.true 'lets_encrypt.accept_terms' && [ $((now - LE_UPDATE)) -ge 43200 ]; then
-        le_renew
-    fi
-    
-    sleep "${WAIT_TIME}"
-done
+echo Start frp as client
+
+exec $FRP_PATH/frpc -c $FRPC_CONF < /dev/null

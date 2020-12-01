40d66b395d19701dff7da331f6b2529f5e4015ff
chong <chong@alex.com.my>
2020-12-02 00:09:57 +0800

    test

--- customfrpc2/data/run.sh
+++ customfrpc2/data/run.sh
@@ -6,6 +6,7 @@ CONFIG_PATH=/data/options.json
 SERVER_IP=$(jq --raw-output '.server_ip' $CONFIG_PATH)
 TOKEN=$(jq --raw-output '.token' $CONFIG_PATH)
 SERVER_PORT=$(jq --raw-output '.server_port' $CONFIG_PATH)
+REMOTE_PORT=$(jq --raw-output '.remote_port' $CONFIG_PATH)
 LOCAL_PORT=$(jq --raw-output '.local_port' $CONFIG_PATH)
 PROXY_NAME=$(jq --raw-output '.proxy_name // empty' $CONFIG_PATH)
 SUBDOMAIN=$(jq --raw-output '.subdomain' $CONFIG_PATH)
@@ -36,6 +37,8 @@ echo "subdomain = $SUBDOMAIN" >> $FRPC_CONF
 echo "type = $TYPE" >> $FRPC_CONF
 echo "local_ip = $LOCAL_IP" >> $FRPC_CONF
 echo "local_port = $LOCAL_PORT" >> $FRPC_CONF
+echo "REMOTE_PORT = $REMOTE_PORT" >> $FRPC_CONF
+
 
 echo Start frp as client
 

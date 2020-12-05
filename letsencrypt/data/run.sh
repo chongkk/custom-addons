fbfab71115e70b194d724e8f62cefd8afe92f792
chong <chong@alex.com.my>
2020-12-05 13:33:48 +0800

    Update run.sh

--- letsencrypt/data/run.sh
+++ letsencrypt/data/run.sh
@@ -19,6 +19,20 @@ WAIT_TIME=$(bashio::config 'seconds')
 
 LE_UPDATE="0"
 
+while true; do
+    if [ ! -f "/ssl/$KEYFILE" ]; then
+      le_new
+      bashio::log.info "Register new certificate"
+    else
+        now="$(date +%s)"
+        if [ $((now - LE_UPDATE)) -ge ${WAIT_TIME} ]; then
+            le_new
+        fi
+        bashio::log.info "Cron Check every ${WAIT_TIME} seconds"
+        sleep "${WAIT_TIME}"
+    fi
+done
+
 function le_new() {
 if [ "${CHALLENGE}" == "dns" ]; then
     bashio::log.info "Selected DNS Provider: ${DNS_PROVIDER}"
@@ -79,17 +93,3 @@ cp "${CERT_DIR_LATEST}fullchain.pem" "/ssl/$CERTFILE"
 
 LE_UPDATE="$(date +%s)"
 }
-
-while true; do
-    if [ ! -f "/ssl/$KEYFILE" ]; then
-      le_new
-      bashio::log.info "Register new certificate"
-    else
-        now="$(date +%s)"
-        if [ $((now - LE_UPDATE)) -ge ${WAIT_TIME} ]; then
-            le_new
-        fi
-        bashio::log.info "Cron Check every ${WAIT_TIME} seconds"
-        sleep "${WAIT_TIME}"
-    fi
-done

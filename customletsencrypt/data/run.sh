3a8d4c3466e333615542840b6b5abbc0a7bdd892
chong <chong@alex.com.my>
2020-12-05 17:34:27 +0800

    Update run.sh

--- customletsencrypt/data/run.sh
+++ customletsencrypt/data/run.sh
@@ -79,17 +79,16 @@ if [ ! -f "/ssl/$KEYFILE" ]; then
 
     cp "${CERT_DIR_LATEST}privkey.pem" "/ssl/$KEYFILE"
     cp "${CERT_DIR_LATEST}fullchain.pem" "/ssl/$CERTFILE"
-    mkdir -p "/ssl/letsencrypt/renewal"
-    cp -rf "$CERT_DIR/renewal/." "/ssl/letsencrypt/renewal/"
+    mkdir -p "/ssl/letsencrypt"
+    cp -rf "$CERT_DIR/." "/ssl/letsencrypt/"
 
         LE_UPDATE="$(date +%s)"
 else
     bashio::log.info "Renewing new certificate"
-    mkdir -p "$CERT_DIR/live/$DOMAINS/"
-    mkdir -p "$CERT_DIR/renewal/"
-    cp "/ssl/$KEYFILE" "$CERT_DIR/live/$DOMAINS/privkey.pem"
-    cp "/ssl/$CERTFILE" "$CERT_DIR/live/$DOMAINS/fullchain.pem"
-    cp -rf "/ssl/letsencrypt/renewal/." "$CERT_DIR/renewal/"
+    #mkdir -p "$CERT_DIR/live/$DOMAINS/"
+    #cp "/ssl/$KEYFILE" "$CERT_DIR/live/$DOMAINS/privkey.pem"
+    #cp "/ssl/$CERTFILE" "$CERT_DIR/live/$DOMAINS/fullchain.pem"
+    cp -rf "/ssl/letsencrypt/." "$CERT_DIR/"
 
     # Gather all domains into a plaintext file
     DOMAIN_ARR=()
